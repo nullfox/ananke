53eef65005db9748139f60ef41984d2a4988a498
Ben Fox <ben@lattice.com>
2021-10-12 11:23:43 -0600

    Publishing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as RPC, Error as RPCError } from './rpc';
 export { default as REST, Error as RESTError } from './rest';
 export { default as Queue } from './queue';
+export { default as GraphQL } from './graphql';
 export { Context, Params, Middleware, Method, Handler } from './interfaces';
